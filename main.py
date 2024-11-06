26180fb5731f2f2f0f5352d1ac2acaf655d84759
CawokG <cawokgonchy@gmail.com>
2024-11-06 15:19:15 +0300

    fix orders name

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from data_manager import *
 from file_manager import *
+from orders_processing import *
 
 
 df = load_data(url='data/inventory.csv')
 df_low_q = get_items_above_q1(df, 'Quantity in Stock')
 write_to_df_report(df_low_q, "low_quantity_report.txt")
+
+days = [5, 6, 7]
+for day in days:
+    print("Day: ", day)
+    orders = load_data(url=f'data/orders_2023-09-0{day}.csv')
+    for ind, order in orders.iterrows():
+        print(order)
+        # if not process_order(df, *order):
+        #     print("")
+    # generate_report(df)
