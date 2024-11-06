ecb4dbb7d442d60c65bc73cc4271a56a0bdba72b
CawokG <cawokgonchy@gmail.com>
2024-11-06 13:49:17 +0300

    teammate1.py: add basic order processing

--- teammate1.py
+++ teammate1.py
@@ -1,12 +1,29 @@
-"""
-2. **Обработка заказов**
-   - Настрой загрузку данных и обновление остатка товаров на складе.
-   - Если заказ превышает количество товара на складе, система должна 
-   либо частично исполнить заказ, либо отклонить его с логированием 
-   (подход выбери самостоятельно).
-"""
+import numpy as np
+import pandas as pd
 
-def 
+
+def process_order(inventory: pd.DataFrame, item_code: int, quantity_ordered: int, date: str):
+    """
+    2. **Обработка заказов**
+        - Настрой загрузку данных и обновление остатка товаров на складе.
+        - Если заказ превышает количество товара на складе, система должна 
+        либо частично исполнить заказ, либо отклонить его с логированием 
+        (подход выбери самостоятельно).
+    """
+    
+    if item_code not in inventory['Item Code'].values:
+        # cancel order
+        # logging: item not found
+        return None
+
+    if inventory.loc[item_code, 'Quantity in Stock'] < quantity_ordered:
+		# cancel order
+        # logging: "not enough in stock \nCode: {item_code}, \tName: {inventory.loc[item_code, 'Name']}, \tQuantity: {inventory.loc[item_code, 'Quantity in Stock']}"
+        return None
+    
+    inventory.loc[item_code, 'Quantity in Stock'] -= quantity_ordered
+    
+    
 
 
 """
